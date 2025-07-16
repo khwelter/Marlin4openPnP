882f1f4a3d834b42c8e47a23eb4c39ffd87eac16
khwelter <khwelter@icloud.com>
2025-07-16 21:58:48 +0200

    adjusted velocity and jerk parameters; changed driver to TMC; working

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -69,6 +69,7 @@
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
   #define MOTHERBOARD BOARD_BTT_OCTOPUS_PRO_V1_1
+  #define NO_USER_FEEDBACK_WARNING
 #endif
 
 // @section serial
@@ -152,19 +153,19 @@
  *          TMC5130, TMC5130_STANDALONE, TMC5160, TMC5160_STANDALONE
  * :['A4988', 'A5984', 'DRV8825', 'LV8729', 'TB6560', 'TB6600', 'TMC2100', 'TMC2130', 'TMC2130_STANDALONE', 'TMC2160', 'TMC2160_STANDALONE', 'TMC2208', 'TMC2208_STANDALONE', 'TMC2209', 'TMC2209_STANDALONE', 'TMC2240', 'TMC2240_STANDALONE', 'TMC2660', 'TMC2660_STANDALONE', 'TMC5130', 'TMC5130_STANDALONE', 'TMC5160', 'TMC5160_STANDALONE']
  */
-#define X_DRIVER_TYPE  A4988
-#define Y_DRIVER_TYPE  A4988
-#define Z_DRIVER_TYPE  A4988
+#define X_DRIVER_TYPE  TMC2130
+#define Y_DRIVER_TYPE  TMC2130
+#define Z_DRIVER_TYPE  TMC2130
 //#define X2_DRIVER_TYPE A4988
 //#define Y2_DRIVER_TYPE A4988
 //#define Z2_DRIVER_TYPE A4988
 //#define Z3_DRIVER_TYPE A4988
 //#define Z4_DRIVER_TYPE A4988
-#define I_DRIVER_TYPE  A4988
-#define J_DRIVER_TYPE  A4988
-#define K_DRIVER_TYPE  A4988
-#define U_DRIVER_TYPE  A4988
-#define V_DRIVER_TYPE  A4988
+#define I_DRIVER_TYPE  TMC2130
+#define J_DRIVER_TYPE  TMC2130
+#define K_DRIVER_TYPE  TMC2130
+#define U_DRIVER_TYPE  TMC2130
+#define V_DRIVER_TYPE  TMC2130
 //#define W_DRIVER_TYPE  A4988
 //#define E0_DRIVER_TYPE A4988
 //#define E1_DRIVER_TYPE A4988
@@ -206,11 +207,11 @@
 #endif
 #ifdef U_DRIVER_TYPE
   #define AXIS7_NAME 'U' // :['U', 'V', 'W']
-  //#define AXIS7_ROTATES
+  #define AXIS7_ROTATES
 #endif
 #ifdef V_DRIVER_TYPE
   #define AXIS8_NAME 'V' // :['V', 'W']
-  //#define AXIS8_ROTATES
+  #define AXIS8_ROTATES
 #endif
 #ifdef W_DRIVER_TYPE
   #define AXIS9_NAME 'W' // :['W']
@@ -1294,7 +1295,14 @@
  * Override with M92 (when enabled below)
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 320, 320, 320, 8.9, 8.9, 320, 8.9, 8.9 }
+/*
+ * base conditions:
+ *  ball screw with 10 mm
+ *  microstepping with 16 steps/step(1.8°) => 3200 steps per revolution => 320 steps/mm
+ * 
+ *  rotational 1,8° pre step => 16 steps/° / 1.8° => 8.8889 steps/ 1.0°
+ */
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 640, 640, 17.7, 17.7, 17.7, 17.7, 17.7, 17.7 }
 
 /**
  * Enable support for M92. Disable to save at least ~530 bytes of flash.
@@ -1306,11 +1314,11 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 300, 300, 300, 300, 300, 300 }
+#define DEFAULT_MAX_FEEDRATE          { 200, 200, 7200, 7200, 7200, 7200, 7200, 7200 }
 
-//#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
+#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
-  #define MAX_FEEDRATE_EDIT_VALUES    { 600, 600, 10, 50 } // ...or, set your own edit limits
+  #define MAX_FEEDRATE_EDIT_VALUES    { 300, 300, 10800, 10800, 10800, 10800, 10800, 10800 } // ...or, set your own edit limits
 #endif
 
 /**
@@ -1319,11 +1327,11 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 300, 300, 300, 300, 300, 300, 300, 300 }
+#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 108000, 108000, 108000, 108000, 108000, 1080000 }
 
-//#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
+#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
-  #define MAX_ACCEL_EDIT_VALUES       { 6000, 6000, 200, 20000 } // ...or, set your own edit limits
+  #define MAX_ACCEL_EDIT_VALUES       { 4500, 4500, 162000, 162000, 162000, 162000, 162000, 162000 } // ...or, set your own edit limits
 #endif
 
 /**
@@ -1334,9 +1342,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          300    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  300    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   300    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          3000    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk limits (mm/s)
@@ -1346,17 +1354,17 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-//#define CLASSIC_JERK
+#define CLASSIC_JERK
 #if ENABLED(CLASSIC_JERK)
   #define DEFAULT_XJERK 10.0
   #define DEFAULT_YJERK 10.0
   #define DEFAULT_ZJERK  0.3
-  #define DEFAULT_EJERK  5.0
-  //#define DEFAULT_IJERK  0.3
-  //#define DEFAULT_JJERK  0.3
-  //#define DEFAULT_KJERK  0.3
-  //#define DEFAULT_UJERK  0.3
-  //#define DEFAULT_VJERK  0.3
+//  #define DEFAULT_EJERK  5.0
+  #define DEFAULT_IJERK  0.3
+  #define DEFAULT_JJERK  0.3
+  #define DEFAULT_KJERK  0.3
+  #define DEFAULT_UJERK  0.3
+  #define DEFAULT_VJERK  0.3
   //#define DEFAULT_WJERK  0.3
 
   //#define TRAVEL_EXTRA_XYJERK 0.0     // Additional jerk allowance for all travel moves
@@ -1792,7 +1800,7 @@
 // For Inverting Stepper Enable Pins (Active Low) use 0, Non Inverting (Active High) use 1
 // :['LOW', 'HIGH']
 #define X_ENABLE_ON LOW
-#define Y_ENABLE_ON HIGH
+#define Y_ENABLE_ON LOW
 #define Z_ENABLE_ON LOW
 #define E_ENABLE_ON LOW // For all extruders
 #define I_ENABLE_ON LOW
@@ -1859,7 +1867,7 @@
  */
 //#define Z_IDLE_HEIGHT Z_HOME_POS
 
-//#define Z_CLEARANCE_FOR_HOMING  4   // (mm) Minimal Z height before homing (G28) for Z clearance above the bed, clamps, ...
+#define Z_CLEARANCE_FOR_HOMING  0   // (mm) Minimal Z height before homing (G28) for Z clearance above the bed, clamps, ...
                                       // You'll need this much clearance above Z_MAX_POS to avoid grinding.
 
 //#define Z_AFTER_HOMING         10   // (mm) Height to move to after homing (if Z was homed)
@@ -1871,10 +1879,10 @@
 // :[-1,1]
 #define X_HOME_DIR -1
 #define Y_HOME_DIR -1
-#define Z_HOME_DIR -1
+#define Z_HOME_DIR -1     // left stepper on gantry
 #define I_HOME_DIR -1
 #define J_HOME_DIR -1
-#define K_HOME_DIR -1
+#define K_HOME_DIR 1      // right stepper on gantry
 #define U_HOME_DIR -1
 #define V_HOME_DIR -1
 //#define W_HOME_DIR -1
@@ -1903,16 +1911,16 @@
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
-#define Z_MIN_POS -90
+#define Z_MIN_POS -100
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 90
+#define Z_MAX_POS 100
 #define I_MIN_POS 0
 #define I_MAX_POS 360
-#define J_MIN_POS -90
-#define J_MAX_POS 90
-#define K_MIN_POS 0
-#define K_MAX_POS 360
+#define J_MIN_POS 0
+#define J_MAX_POS 360
+#define K_MIN_POS -100
+#define K_MAX_POS 100
 #define U_MIN_POS 0
 #define U_MAX_POS 360
 #define V_MIN_POS 0
@@ -1935,11 +1943,11 @@
   #define MIN_SOFTWARE_ENDSTOP_X
   #define MIN_SOFTWARE_ENDSTOP_Y
   #define MIN_SOFTWARE_ENDSTOP_Z
-  #define MIN_SOFTWARE_ENDSTOP_I
-  #define MIN_SOFTWARE_ENDSTOP_J
+//  #define MIN_SOFTWARE_ENDSTOP_I
+//  #define MIN_SOFTWARE_ENDSTOP_J
   #define MIN_SOFTWARE_ENDSTOP_K
-  #define MIN_SOFTWARE_ENDSTOP_U
-  #define MIN_SOFTWARE_ENDSTOP_V
+//  #define MIN_SOFTWARE_ENDSTOP_U
+//  #define MIN_SOFTWARE_ENDSTOP_V
   #define MIN_SOFTWARE_ENDSTOP_W
 #endif
 
@@ -1949,11 +1957,11 @@
   #define MAX_SOFTWARE_ENDSTOP_X
   #define MAX_SOFTWARE_ENDSTOP_Y
   #define MAX_SOFTWARE_ENDSTOP_Z
-  #define MAX_SOFTWARE_ENDSTOP_I
-  #define MAX_SOFTWARE_ENDSTOP_J
+//  #define MAX_SOFTWARE_ENDSTOP_I
+//  #define MAX_SOFTWARE_ENDSTOP_J
   #define MAX_SOFTWARE_ENDSTOP_K
-  #define MAX_SOFTWARE_ENDSTOP_U
-  #define MAX_SOFTWARE_ENDSTOP_V
+//  #define MAX_SOFTWARE_ENDSTOP_U
+//  #define MAX_SOFTWARE_ENDSTOP_V
   #define MAX_SOFTWARE_ENDSTOP_W
 #endif
 
@@ -2370,7 +2378,7 @@
 #define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (4*60), (50*60), (50*60), (4*60), (50*60), (4*60) }
 
 // Edit homing feedrates with M210 and MarlinUI menu items
-//#define EDITABLE_HOMING_FEEDRATE
+#define EDITABLE_HOMING_FEEDRATE
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -933,7 +933,7 @@
 
 //#define SENSORLESS_BACKOFF_MM  { 2, 2, 0 }  // (linear=mm, rotational=°) Backoff from endstops before sensorless homing
 
-#define HOMING_BUMP_MM      { 2, 2, 0, 0, 0, 0, 0, 0 }       // (linear=mm, rotational=°) Backoff from endstops after first bump
+#define HOMING_BUMP_MM      { 1, 1, 1, 1, 1, 1, 1, 1 }       // (linear=mm, rotational=°) Backoff from endstops after first bump
 #define HOMING_BUMP_DIVISOR { 2, 2, 2, 2, 2, 2, 2, 2 }       // Re-Bump Speed Divisor (Divides the Homing Feedrate)
 
 //#define HOMING_BACKOFF_POST_MM { 2, 2, 2 }  // (linear=mm, rotational=°) Backoff from endstops after homing
